25e275da3d76b96bc327da6ad3f06d810cd2aedf
titedog <71283197+titedog@users.noreply.github.com>
2022-05-23 18:36:43 -0400

    Update main.lua

--- main.lua
+++ main.lua
@@ -323,11 +323,14 @@ function library:Window(title: string)
 	function window:Button(text)
 		local Frame_5 = Instance.new("Frame")
 		local TextLabel_2 = Instance.new("TextLabel")
+		local btn = Instance.new("TextButton")
 
+		Frame_5.Parent = ScrollingFrame
 		Frame_5.BackgroundColor3 = Color3.fromRGB(157, 144, 111)
 		Frame_5.BackgroundTransparency = 1.000
-		Frame_5.Size = UDim2.fromScale(0.8, 0.3)
+		Frame_5.Size = UDim2.new(0, 239, 0, 30)
 
+		TextLabel_2.Parent = Frame_5
 		TextLabel_2.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
 		TextLabel_2.BackgroundTransparency = 1.000
 		TextLabel_2.Position = UDim2.new(0.0368297361, 0, 0.178787738, 0)
@@ -336,10 +339,13 @@ function library:Window(title: string)
 		TextLabel_2.Text = text
 		TextLabel_2.TextColor3 = Color3.fromRGB(176, 176, 176)
 		TextLabel_2.TextSize = 13.000
-		TextLabel_2.TextXAlignment = Enum.TextXAlignment.Center
+		TextLabel_2.TextXAlignment = Enum.TextXAlignment.Left
 		
-		TextLabel_2.Parent = Frame_5
-		Frame_5.Parent = ScrollingFrame
+		btn.Size = UDim2.fromScale(1, 1)
+		btn.BackgroundColor = Color3.fromRGB(30, 30, 30)
+		btn.Parent = Frame_5
+		btn.ZIndex = Frame_5.ZIndex + 1
+		TextLabel_2.ZIndex = btn.ZIndex + 1
 	end
 
 	function window:Slider(text, options, callback)
